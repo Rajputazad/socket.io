18dc18aff422f36a7cba343a41f03ccf3a186095
Your Name <you@example.com>
2023-02-07 14:31:18 +0530

    update

--- index.js
+++ index.js
@@ -5,26 +5,13 @@ const socketio= require("socket.io")
 const server = http.createServer(app);
 const port=3000
 // const io = socketio(server);
-const cors = require("cors");
-app.use(
-  cors({
-    origin: ["http://localhost:4200", "*"],
-    credentials: true,
-  })
-);
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id,authorization");
 
-  next();
-});
 const io = require('socket.io')(server, {
   cors: {origin : '*'}
 });
 
 io.on('connection', client => {
-  client.on('message', data => { console.log(data);
+  client.on('message', data => { console.log(data + "add");
     io.emit('message', `${client.id.substr(0, 2)}: ${data}`)
 }
   );
